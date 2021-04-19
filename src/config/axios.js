c1f0b6953a1a2cf212267baa051c2cf7c3a57d18
Hector <hectorvives@gmail.com>
2021-04-18 22:22:25 -0500

    updates

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 require('dotenv').config({ path: 'app-variables.env' });
 
 const clienteAxios = axios.create({
-    baseURL : 'http://ec2-34-230-47-145.compute-1.amazonaws.com:8080'
+    baseURL : 'ec2-18-234-108-166.compute-1.amazonaws.com:4000'
 });
 
 export default clienteAxios;
